b36234302ff11aa9c26bf0109d0f2aa3e76761bf
Ethan Hansen <hanseneg@hotmail.com>
2021-04-07 15:11:08 -0600

    commit12

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 
+//step 1 connecting to heroku
+const PORT = process.env.PORT || 8000
 
 //middleware for every request
 //looks for request body and turns it into req.body
@@ -10,8 +12,10 @@ const mongoose = require('mongoose')
 app.use(express.json())
 app.use(morgan('dev'))
 
+
+//step 2 connect to heroku
 //connect to DB
-mongoose.connect('mongodb://localhost:27017/bountiesdb',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bountiesdb',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -30,7 +34,22 @@ app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
 })
 
+
+//step 3 connect to heroku
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
+
 //server listen
-app.listen(8000, () => {
-    console.log('The server is running on Port 8000')
+app.listen(PORT, () => {
+    console.log(`The server is running on Port ${PORT}`)
 })
+
+
+//The mongolab:sandbox addon is no longer available. 
+//What you can do instead is connect directly to atlas without an addon. 
+//Just make sure to add the config vars 
+//(in the heroku dashboard go to personal > app > settings > config vars). 
+//The config vars are the MONGODB_URI and the NODE.ENV===production (the ones from the server.js file). 
+//The next tutorial in this playlist shows how it's done.
+
