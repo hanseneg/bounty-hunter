38fd17f9a5e5f21915b082f8d9f769815bfc8a5f
Ethan Hansen <hanseneg@hotmail.com>
2021-04-08 12:00:37 -0600

    commit14

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 
+const path = require("path")
+
 //step 1 connecting to heroku
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 //const MONGODB_URI = 'mongodb+srv://hanseneg:bountyhunter@cluster0.nbyij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 //middleware for every request
 //looks for request body and turns it into req.body
 //fires on every request
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(express.json())
 app.use(morgan('dev'))
 
 
 //step 2 connect to heroku
 //connect to DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bountiesdb',
+mongoose.connect(process.env.MONGODB_URI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -38,20 +43,15 @@ app.use((err, req, res, next) => {
 
 
 //step 3 connect to heroku
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+// }
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 
 //server listen
 app.listen(PORT, () => {
     console.log(`The server is running on Port ${PORT}`)
 })
-
-
-//The mongolab:sandbox addon is no longer available. 
-//What you can do instead is connect directly to atlas without an addon. 
-//Just make sure to add the config vars 
-//(in the heroku dashboard go to personal > app > settings > config vars). 
-//The config vars are the MONGODB_URI and the NODE.ENV===production (the ones from the server.js file). 
-//The next tutorial in this playlist shows how it's done.
-
